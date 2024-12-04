4a670ec61e4e781c25f8f58f2391a2121a14c46a
Alexander King <alexhking@gmail.com>
2024-12-04 15:05:52 -0500

    New note component with optional custom emoji. Cleaned up all instances of note component

--- components/note.tsx
+++ components/note.tsx
@@ -3,28 +3,36 @@ import clsx from "clsx";
 import { PropsWithChildren } from "react";
 
 type NoteProps = PropsWithChildren & {
-  type?: "note" | "danger" | "warning" | "success";
+  type?: "note" | "danger" | "warning";
+  emoji?: string; // Optional emoji override
 };
 
-export default function Note({ children, type = "note" }: NoteProps) {
+export default function Note({ children, type = "note", emoji }: NoteProps) {
   const noteClassNames = clsx({
-    "bg-gray-900 border-gray-800": type == "note",
-    "dark:bg-red-950 bg-red-100 border-red-200 dark:border-red-900":
-      type === "danger",
-    "dark:bg-orange-950 bg-orange-100 border-orange-200 dark:border-orange-900":
-      type === "warning",
-    "dark:bg-green-950 bg-green-100 border-green-200 dark:border-green-900":
-      type === "success",
+    "bg-gray-900 border-gray-800": type === "note",
+    "bg-yellow-950 border-yellow-900": type === "warning",
+    "bg-red-100 border-red-200": type === "danger",
   });
+
+  // Default emojis by note type
+  const typeEmojis: Record<"note" | "warning" | "danger", string> = {
+    note: "🤔",
+    warning: "😬",
+    danger: "😱",
+  };
+
+  // Use the overridden emoji if provided; otherwise, use the default
+  const displayedEmoji = emoji || typeEmojis[type];
 
   return (
     <div
       className={cn(
-        "border rounded-md py-0.5 px-3.5 text-sm tracking-wide",
+        "note-container border rounded-md p-3.5 text-sm tracking-wide flex items-start",
         noteClassNames
       )}
     >
-      {children}
+      <span className="note-emoji">{displayedEmoji}</span>
+      <aside className="note-aside">{children}</aside>
     </div>
   );
 }

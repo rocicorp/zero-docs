bfd47c6db7335676cbe39a22f9740a55e6434f59
JulianKominovic <juliankominovic@gmail.com>
2025-05-09 22:03:20 -0300

    fix: navbar max width

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,7 +14,7 @@ export const NAVLINKS = [
 
 export function Navbar() {
   return (
-    <nav className="w-full sticky top-0 z-50 md:px-8 px-2 backdrop-filter backdrop-blur-xl bg-opacity-5 h-[72px] grid grid-cols-[auto_1fr_auto] md:grid-cols-[auto_1fr] gap-2 items-center">
+    <nav className="w-full sticky top-0 z-50 md:px-8 px-2 backdrop-filter backdrop-blur-xl bg-opacity-5 h-[72px] grid grid-cols-[auto_1fr_auto] md:grid-cols-[auto_1fr] gap-2 items-center max-w-[1250px] mx-auto">
       <SheetLeftbar />
       <Logo />
       <div className="flex items-center justify-end gap-2">
@@ -27,7 +27,10 @@ export function Navbar() {
 
 function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-3 justify-center ">
+    <Link
+      href="/"
+      className="flex items-center gap-3 relative xs:absolute md:relative left-1/2 -translate-x-1/2 justify-center "
+    >
       <ZeroAlphaLogo className="w-full h-8" />
     </Link>
   );

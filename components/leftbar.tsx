1f1135165710ab5f9ac4884a39d6301e079f8b05
Alexander King <alexhking@gmail.com>
2025-05-19 21:54:54 -0400

    Removing calculated height of left bar

--- components/leftbar.tsx
+++ components/leftbar.tsx
@@ -15,7 +15,7 @@ import Link from 'next/link';
 
 export function Leftbar() {
   return (
-    <aside className="md:flex hidden flex-[1] min-w-[230px] sticky top-16 h-[calc(100vh-64px)] flex-col py-4">
+    <aside className="md:flex hidden flex-[1] min-w-[230px] sticky top-16 flex-col py-4">
       <ScrollArea className="h-full pr-2">
         <DocsMenu />
       </ScrollArea>

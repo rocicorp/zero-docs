f18fd0c6a008d925dc742008ffbe1f89f397f6c2
Chase Adams <c@cadams.io>
2025-07-16 16:36:08 -0700

    fix: revert uncoordinated changes

--- components/toc.tsx
+++ components/toc.tsx
@@ -14,7 +14,7 @@ export default async function Toc({path}: {path: string}) {
           <>
             <h3 className="font-semibold text-sm">On this page</h3>
             <ScrollArea className="pb-4 h-full pt-0.5">
-              <div className="flex flex-col gap-2.5 text-sm text-muted-foreground ml-0.5">
+              <div className="flex flex-col gap-2.5 text-sm dark:text-neutral-300/85 text-neutral-500/85 ml-0.5">
                 {tocs.map(({href, level, text}) => (
                   <ActiveHashLink
                     key={href}

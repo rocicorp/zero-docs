2680cbfaeac5bad0663fea84d1ca0e9685c5ee27
Chase Adams <c@cadams.io>
2025-11-21 12:13:43 -0700

    chore: added caching

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,37 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*.txt',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, s-maxage=86400, stale-while-revalidate=43200',
+          },
+        ],
+      },
+      {
+        source: '/:path*.md',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, s-maxage=86400, stale-while-revalidate=43200',
+          },
+        ],
+      },
+      {
+        source: '/:path*.mdx',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, s-maxage=86400, stale-while-revalidate=43200',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

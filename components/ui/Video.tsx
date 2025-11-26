3440b0be41abb8b3e2217adc6293a7faea84d03c
Chase Adams <c@cadams.io>
2025-11-26 08:15:41 -0700

    docs: added (most of the) video animations

--- components/ui/Video.tsx
+++ components/ui/Video.tsx
@@ -3,23 +3,39 @@
 import React from 'react';
 
 type VideoProps = {
-  src: string; // Path to the video file
-  alt: string; // Accessibility text
+  src?: `/${string}`; // Path to the video file
+  alt?: string; // Accessibility text
+  animation?: boolean;
 };
 
-const Video: React.FC<VideoProps> = ({src, alt}) => {
+const Video: React.FC<VideoProps> = ({src, alt, animation}) => {
   if (!src) {
     console.error("Video component requires a 'src' property.");
     return null;
   }
 
+  if (!alt) {
+    console.error("Video component requires an 'alt' property.");
+    return null;
+  }
+
+  if (!src.startsWith('/')) {
+    console.error('Video src must be a local path');
+    return null;
+  }
+
+  const videoSrc = `https://d2nzlypqdo5xbb.cloudfront.net${src}`;
+
   return (
     <div style={{maxWidth: '100%', margin: '1rem 0'}}>
       <video
-        src={src}
-        controls
+        src={videoSrc}
+        controls={!animation}
         style={{width: '100%', borderRadius: '8px'}}
         aria-label={alt}
+        autoPlay={Boolean(animation)}
+        loop={Boolean(animation)}
+        muted={Boolean(animation)}
       >
         <track kind="captions" />
         Your browser does not support the video tag.

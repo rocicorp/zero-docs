442a607fe7ec6509ad147a89633aea8a882f9597
Alexander King <alexhking@gmail.com>
2026-01-30 12:17:16 -0500

    Switching to static OG image for now

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import {IntroductionLanding} from '@/components/IntroductionLanding';
 import {Metadata} from 'next';
 
 const title = 'Zero';
-const description = 'General Purpose Sync for the Web';
+const description = 'Instant Queries by Default';
 
 export const metadata: Metadata = {
   title,
@@ -10,13 +10,13 @@ export const metadata: Metadata = {
   openGraph: {
     title,
     description,
-    images: `/api/og?logo=zero`,
+    images: '/opengraph-image.png',
   },
   twitter: {
     card: 'summary_large_image',
     title,
     description,
-    images: `/api/og?logo=zero`,
+    images: '/opengraph-image.png',
   },
 };
 

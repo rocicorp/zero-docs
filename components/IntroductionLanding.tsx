6b4f35dbd061c7ce32a1addeea7eaf859ff81802
Aaron Boodman <aaron@aaronboodman.com>
2026-07-06 21:50:27 -1000

    quote quote (#307)

--- components/IntroductionLanding.tsx
+++ components/IntroductionLanding.tsx
@@ -487,8 +487,7 @@ export function IntroductionLanding({
             <div className="testimonial-card">
               <div className="testimonial-quote">
                 <p>
-                  As a general sync engine, Zero lets us change our data model
-                  without touching infrastructure.
+                  Zero’s initial announcement inspired us to build our own sync engine. It worked, but was a lot to maintain. We switched to Zero itself and are happily shipping again.
                 </p>
               </div>
               <div className="testimonial-author">
